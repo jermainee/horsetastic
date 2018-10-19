42c6cadf1386a650ffff0f08473a623b85b36ce8
Jermaine Ernst <jermaine.ernst@icloud.com>
2018-10-19 21:52:48 +0200

    Implement react-native-maps into RideView

--- src/RideView/RideView.tsx
+++ src/RideView/RideView.tsx
@@ -1,22 +1,78 @@
 import React, { Component } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from 'react-native-maps';
+
 
 interface IRideViewProps {
     toggleView: () => void;
 }
 
-export default class RideView extends Component<IRideViewProps> {
+interface IRideViewState {
+    latitude: number;
+    longitude: number
+    altitude: number | null;
+    speed: number | null;
+    error: string | null;
+}
+
+export default class RideView extends Component<IRideViewProps, IRideViewState> {
 
     // TODO: Show speed
     // TODO: Show acceleration?
     // TODO: Implement SOS-System
     // TODO: Show Map
     // TODO: Show Distance
+    // TODO: Implement route logger
+
+    public componentWillMount() {
+        this.setState({
+            latitude: 0,
+            longitude: 0,
+            error: null
+        });
+    }
+
+    public componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    altitude: position.coords.altitude,
+                    speed: position.coords.speed
+                });
+            },
+            (error) => this.setState({error: error.message}),
+            {
+                enableHighAccuracy: false,
+                timeout: 200000,
+                maximumAge: 1000
+            }
+        );
+    }
 
     public render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>Ride</Text>
+
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: this.state.latitude,
+                        longitude: this.state.latitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                >
+                    <Marker
+                        coordinate={{
+                            latitude: this.state.latitude,
+                            longitude: this.state.longitude
+                        }}
+                        image={require('../images/horse-icon.svg')}
+                    />
+                </MapView>
 
                 <Button
                     onPress={this.props.toggleView}
@@ -46,4 +102,7 @@ const styles = StyleSheet.create({
         color: '#333333',
         marginBottom: 5,
     },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    }
 });
